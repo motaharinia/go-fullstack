3fc5a9d80e2dc77ff4915aacfb19bafc7057cca5
Mostafa Motaharinia <eng.motahari@gmail.com>
2021-11-13 00:15:46 +0330

    initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+//https://levelup.gitconnected.com/crud-restful-api-with-go-gorm-jwt-postgres-mysql-and-testing-460a85ab7121
+
+import "github.com/motaharinia/go-fullstack/api"
+
+func main() {
+	api.Run()
+}
